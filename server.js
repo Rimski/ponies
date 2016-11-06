7567acc41c0ec76baeca677bdf8f19c34ce9ef6b
Tanner S.R. Ward <tsrward@wrst.ninja>
2016-11-05 21:21:24 -0400

    corrected path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.port || 8800;
 var path = require("path");
 var cors = require("cors");
 app.use(cors());
-app.use(express.static(path.join(__dirname, "..", "MyLittleBrony")))
+app.use(express.static(path.join(__dirname, "..", "ponies")))
 app.listen(port, function() {
     console.log("Server running on " + port);
 });
