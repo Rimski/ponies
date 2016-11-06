187369f4a2a7ce789052d415f1c3dc698f7d8dbe
Tanner Ward <hoserehh@gmail.com>
2016-11-05 19:17:09 -0600

    makeing own repository

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var port = process.env.port || 8800;
+var path = require("path");
+var cors = require("cors");
+app.use(cors());
+app.use(express.static(path.join(__dirname, "..", "MyLittleBrony")))
+app.listen(port, function() {
+    console.log("Server running on " + port);
+});
